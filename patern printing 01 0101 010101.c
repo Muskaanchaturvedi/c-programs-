b569002c353553b70169c467e4b99b6670185f96
Muskaanchaturvedi <76175842+Muskaanchaturvedi@users.noreply.github.com>
2020-12-27 01:30:38 +0530

    Add files via upload

---
+++ patern printing 01 0101 010101.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+    int i, j;
+
+    for(i=1;1<=6;i++)
+    {
+        for (j=1;j<=i;j++)
+        {
+            printf("01");
+        }
+        printf("\t");
+    }
+    return(0);
+
+}
